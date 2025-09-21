70787642471b70460878bdb76dc442aa472626a9
q1110 <zeratuliche@gmail.com>
2025-09-21 17:07:33 +0300

    ...

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/ZeroTheorem/love_counter_bot.git
+
+go 1.25.1
+
+require (
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.32 // indirect
+	gopkg.in/telebot.v4 v4.0.0-beta.5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	//"fmt"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+
+	_ "github.com/mattn/go-sqlite3"
+	tele "gopkg.in/telebot.v4"
+)
+
+var (
+	alexCount  int = 0
+	alenaCount int = 0
+)
+
+func main() {
+	// create telegramm connection
+	pref := tele.Settings{
+		Token:     os.Getenv("TOKEN"),
+		Poller:    &tele.LongPoller{Timeout: 10 * time.Second},
+		ParseMode: tele.ModeHTML,
+	}
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	// create database connection
+	db, err := sqlx.Connect("sqlite3", "./love.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	_, err = db.Exec(
+		`CREATE TABLE IF NOT EXISTS love_counter (
+   name TEXT PRIMARY KEY,
+   count INTEGER)
+`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec(
+		`CREATE TABLE IF NOT EXISTS love_counter (
+ 		  name TEXT PRIMARY KEY,
+  		  count INTEGER)`)
+	var counts []int
+	db.Select(&counts, "SELECT count FROM love_counter;")
+	alexCount = counts[0]
+	alenaCount = counts[1]
+	if err != nil {
+		log.Fatal(err)
+	}
+	b.Handle("/start", func(c tele.Context) error {
+		return c.Send("Hello! Я помогу вам определить, кто кого больше любит)))")
+	})
+
+	b.Handle(tele.OnText, func(c tele.Context) error {
+		msg := c.Message().Text
+		if strings.Contains(msg, "люблю") && strings.Contains(msg, "тебя") {
+			if c.Chat().Username == "qb1110" {
+				_, err := db.Exec("UPDATE love_counter SET count = count + 1 WHERE name = 'Alex';")
+				if err != nil {
+					log.Fatal(err)
+				}
+				alexCount += 1
+				return c.Send(fmt.Sprintf(`❤️
+
+<i>Алексеюшка</i>: <b>%v</b>
+<i>Веснушка</i>: <b>%v</b>
+		`, alexCount, alenaCount))
+			} else {
+				_, err := db.Exec("UPDATE love_counter SET count = count + 1 WHERE name = 'Alena';")
+				if err != nil {
+					log.Fatal(err)
+				}
+				alenaCount += 1
+				return c.Send(fmt.Sprintf(`❤️
+
+<i>Алексеюшка</i>: <b>%v</b>
+<i>Веснушка</i>: <b>%v</b>
+		`, alexCount, alenaCount))
+			}
+		}
+		return nil
+	})
+
+	b.Start()
+}
